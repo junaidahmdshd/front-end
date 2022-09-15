2d63983fc369d6d05b70f18c83d2c2ec5bf6ffe9
junaidahmdshd <junaidahmdshd.com>
2022-09-15 15:22:08 +0500

    message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import Home from "./Home";
 import TrackingPage from "./TrackingPage";
+import Home from "./Home";
 
 const App = () => {
   return (
